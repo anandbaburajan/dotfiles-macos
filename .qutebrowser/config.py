ab682d4f249ffaa649e809b2995eaf1f845888ab
anandbaburajan <anandbaburajan@gmail.com>
2022-10-06 12:25:09 +0530

    prev/next tab bindings

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -2,4 +2,8 @@ c.url.searchengines = {
     'DEFAULT':  'https://google.com/search?hl=en&q={}',
 }
 c.auto_save.session = True
+c.bindings.commands['normal'] = {
+    'K': 'tab-next',
+    'J': 'tab-prev',
+}
 config.load_autoconfig()
